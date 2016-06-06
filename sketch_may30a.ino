d4f435a404757569069d25acd6ad32bc2bf14351
Yiaannn <Yiaannn@gmail.com>
2016-06-06 12:17:50 +0000

    commit inicial
    primeiro protótipo da estação meteorológica

---
+++ sketch_may30a.ino
@@ -0,0 +1,77 @@
+const int LMPIN= 3; //the analog pin the TMP36's Vout (sense) pin is connected to
+                        //the resolution is 10 mV / degree centigrade with a
+                        //500 mV offset to allow for negative temperatures
+const int DHTPIN= 2;
+
+#include "DHT.h"
+DHT dht(DHTPIN, DHT22);
+
+
+void setup(){
+  Serial.begin(9600);
+  dht.begin();
+}
+ 
+void loop(){                     // run over and over again
+  delay(1000);
+  read_lm();
+  delay(1000);
+  read_dht();
+}
+
+void read_dht(){
+  
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
+}
+
+void read_lm(){
+  
+  int output = analogRead(LMPIN);  
+  
+  // converting that reading to voltage, for 3.3v arduino use 3.3
+  float voltage = output * 5;
+  voltage /= 1024.0; 
+  
+  // print out the voltage
+  Serial.print(voltage); Serial.println(" volts");
+  
+  // now print out the temperature
+  float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
+                                               //to degrees ((voltage - 500mV) times 100)
+  Serial.print(temperatureC); Serial.println(" degrees C");
+  
+  // now convert to Fahrenheit
+  float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+  Serial.print(temperatureF); Serial.println(" degrees F");
+}
